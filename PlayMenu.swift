c75ce55727fef8d6d1508db7f2f992db825737e0
u3578231 <98687796+u3578231@users.noreply.github.com>
2023-10-29 11:03:07 +0800

    Update PlayMenu.swift with AR

--- PlayMenu.swift
+++ PlayMenu.swift
@@ -16,6 +16,9 @@ struct PlayMenu: View {
     @State private var navigateToOverview = false
     @State private var showMenu = false
     @State private var navigateToDictionary = false
+    @State private var showBackButton = true
+    @State private var showProfilePicture = true
+    
     var body: some View {
         ZStack {
             Image("background")
@@ -25,25 +28,28 @@ struct PlayMenu: View {
             VStack {
                 List {
                     Section(header: Text("Game Mode")) {
-                        NavigationLink(destination: generateRandomNoView(shuffle_question_set: 0,  username: username)) {
+                        NavigationLink(destination: generateRandomNoView(shuffle_question_set: 0, username: username)) {
                             Text("Time Limit")
                         }
-                        NavigationLink(destination: generateRandomNoView_no_time_limit(shuffle_question_set: 0,  username: username)) {
+                        NavigationLink(destination: generateRandomNoView_no_time_limit(shuffle_question_set: 0, username: username)) {
                             Text("No Time Limit")
                         }
                         NavigationLink(destination: generateRandomNoView_time_limit_heart_attack(username: username)) {
                             Text("Time Limit_heart_attack")
                         }
+                        NavigationLink(destination: AR_View()) {
+                            Text("AR")
+                        }
                     }
                     
                     Section(header: Text("Learning")) {
-                        NavigationLink(destination: DictionaryView(navigateToPlayMenu: $navigateToPlayMenu,username: username)){
+                        NavigationLink(destination: DictionaryView(navigateToPlayMenu: $navigateToPlayMenu, username: username)) {
                             Text("Dictionary")
                         }
-                        NavigationLink(destination: ChartView(username: username)){
+                        NavigationLink(destination: ChartView(username: username)) {
                             Text("Chart")
                         }
-                        NavigationLink(destination: OverviewView(showMenu: $showMenu, navigateToPlayMenu: $navigateToPlayMenu, username: username), isActive: $navigateToOverview){
+                        NavigationLink(destination: OverviewView(showMenu: $showMenu, navigateToPlayMenu: $navigateToPlayMenu, username: username), isActive: $navigateToOverview) {
                             Text("Overview")
                         }
                     }
@@ -53,11 +59,9 @@ struct PlayMenu: View {
                 .navigationBarBackButtonHidden(true)
                 .navigationBarItems(
                     leading: backButton,
-                    trailing: NavigationLink(destination: ProfileView(username: username)) {
-                        Image(systemName: "person.circle")
-                            .font(.system(size: 24))
-                    }
+                    trailing: showProfilePicture ? profileButton : nil
                 )
+                .navigationBarHidden(navigateToPlayMenu || navigateToOverview)
             }
             
             NavigationLink(destination: RegisterView(), isActive: $navigateToRegisterView) {
@@ -73,6 +77,15 @@ struct PlayMenu: View {
             Label("Back", systemImage: "chevron.left")
         }
         .padding()
+        .opacity(showBackButton ? 1.0 : 0.0)
+        .disabled(!showBackButton)
+    }
+    
+    private var profileButton: some View {
+        NavigationLink(destination: ProfileView(username: username)) {
+            Image(systemName: "person.circle")
+                .font(.system(size: 24))
+        }
     }
 }
 struct PlayMenu_Previews: PreviewProvider {
