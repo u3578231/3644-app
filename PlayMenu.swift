a367ff71993f35dcddbb702255dbebbf338827da
u3578231 <98687796+u3578231@users.noreply.github.com>
2023-11-16 23:11:38 +0800

    Update PlayMenu.swift

--- PlayMenu.swift
+++ PlayMenu.swift
@@ -17,184 +17,66 @@ struct PlayMenu: View {
     @State private var navigateToDictionary = false
     @State private var showBackButton = true
     @State private var showProfilePicture = true
+    @State private var time = Timer.publish(every: 0.1, on: .current, in: .tracking).autoconnect()
+    @State private var show = false
     
     var body: some View {
-        ZStack {
-            Image("background")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack {
-                VStack{
-                    Text("Play Menu")
-                        .font(.title)
-                }
-                List {
-                    Section(header: Text("Game Mode")
-                        .font(.system(size: 26))
-                        .bold()
-                        .foregroundColor(.red)
-                    ) {
-                        NavigationLink(destination: generateRandomNoView(shuffle_question_set: 0, username: username)) {
-                            HStack {
-                                Image("time limit easy")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(width: 150, height: 150) // Adjust the size as needed
-                                
-                                VStack(alignment: .leading) {
-                                    Text("Mode: Time Limit Easy")
-                                        .foregroundColor(.red)
-                                        .bold()
-                                    Spacer()
-                                    
-                                    HStack {
-                                        Text("Difficulty: ")
-                                            .foregroundColor(.red)
-                                        Image(systemName: "star.fill")
-                                            .foregroundColor(.yellow)
-                                        Image(systemName: "star.fill")
-                                            .foregroundColor(.yellow)
+        ZStack(alignment: .top) {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack {
+                    GeometryReader { g in
+                        Image("learning")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: g.size.width * 0.8)
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                            .offset(y: g.frame(in: .global).minY > 0 ? -g.frame(in: .global).minY : 0)
+                            .frame(height: g.frame(in: .global).minY > 0 ? UIScreen.main.bounds.height / 2.6 + g.frame(in: .global).minY : UIScreen.main.bounds.height / 2.6)
+                            .onReceive(self.time) { _ in
+                                let y = g.frame(in: .global).minY
+                                if -y > (UIScreen.main.bounds.height / 2.6) - 50 {
+                                    withAnimation {
+                                        self.show = true
+                                    }
+                                } else {
+                                    withAnimation {
+                                        self.show = false
                                     }
                                 }
-                                .padding()
-                                .background(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.blue, lineWidth: 2)
-                                        .frame(width: 370, height: 150)
-                                        .padding(.leading,178)
-                                )
                             }
-                        }
-                        NavigationLink(destination: generateRandomNoView_no_time_limit(shuffle_question_set: 0, username: username)) {
-                            HStack {
-                                Image("no time limit")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(width: 150, height: 150) // Adjust the size as needed
-                                
-                                VStack(alignment: .leading) {
-                                    Text("Mode: No Time Limit")
-                                        .foregroundColor(.red)
-                                        .bold()
-                                    Spacer()
-                                    
-                                    HStack {
-                                        Text("Difficulty: ")
-                                            .foregroundColor(.red)
-                                        Image(systemName: "star.fill")
-                                            .foregroundColor(.yellow)
-                                    }
-                                }
-                                .padding()
-                                .background(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.blue, lineWidth: 2)
-                                        .frame(width: 370, height: 150)
-                                        .padding(.leading,190)
-                                )
-                            }
-                        }
-                        NavigationLink(destination: generateRandomNoView_time_limit_heart_attack(username: username)) {
-                            HStack {
-                                Image("time limit hard")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(width: 150, height: 150) // Adjust the size as needed
-                                
-                                VStack(alignment: .leading) {
-                                    Text("Mode: Time Limit Heart attack/Hard")
-                                        .foregroundColor(.red)
-                                        .bold()
-                                    
-                                    Spacer()
-                                    
-                                    HStack {
-                                        Text("Difficulty: ")
-                                            .foregroundColor(.red)
-                                        Image(systemName: "star.fill")
-                                            .foregroundColor(.yellow)
-                                        Image(systemName: "star.fill")
-                                            .foregroundColor(.yellow)
-                                        Image(systemName: "star.fill")
-                                            .foregroundColor(.yellow)
-                                    }
-                                }
-                                .padding()
-                                .background(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.blue, lineWidth: 2)
-                                        .frame(width: 370, height: 150)
-                                        .padding(.leading,72)
-                                )
-                            }
-                        }
-                        NavigationLink(destination: AR_View()) {
-                            HStack(spacing: 50) {
-                                Image("AR")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(width: 150, height: 150)
-                                
-                                VStack(alignment: .leading) {
-                                    Text("Mode: AR")
-                                        .foregroundColor(.red)
-                                        .bold()
-                                    Spacer()
-                                    
-                                    HStack {
-                                        Text("Difficulty: ")
-                                            .foregroundColor(.red)
-                                    }
-                                }
-                                .padding()
-                                .background(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.blue, lineWidth: 2)
-                                        .frame(width: 318, height: 150)
-                                        .padding(.leading,214)
-                                )
-                            }
-                            .padding()
+                    }
+                    .frame(height: UIScreen.main.bounds.height / 2.6)
+                    Spacer()
+                    VStack(spacing: 20){
+                        ForEach(data){i in
+                            PlayItemView(data: i, username:username, navigateToPlayMenu: $navigateToPlayMenu)
                         }
                     }
-                    
-                    Section(header: Text("Learning")
-                        .font(.system(size: 26))
-                        .foregroundColor(.red)
-                        .bold()) {
-                        NavigationLink(destination: DictionaryView(navigateToPlayMenu: $navigateToPlayMenu, username: username)) {
-                            Text("Dictionary")
-                        }
-                        NavigationLink(destination: ChartView(username: username)) {
-                            Text("Chart")
-                        }
-                        NavigationLink(destination: OverviewView(showMenu: $showMenu, navigateToPlayMenu: $navigateToPlayMenu, username: username), isActive: $navigateToOverview) {
-                            Text("Overview")
-                        }
-                    }
+                    .padding(.top)
+                Spacer()
                 }
-                .listRowBackground(Color.blue.opacity(0.2))
-                .scrollContentBackground(.hidden)
-                .padding(.top, 130) // Adjust the top padding as needed
-                
-                Spacer() // Add a spacer to push the lists to the top
-                    .navigationTitle("Play Menu")
-                .navigationBarBackButtonHidden(true)
-                .navigationBarItems(
-                    leading: backButton,
-                    trailing: showProfilePicture ? profileButton : nil
-                )
-                .navigationBarHidden(navigateToPlayMenu || navigateToOverview)
             }
-            
+            if self.show {
+            }
+        }
+        .navigationBarItems(
+            leading: backButton,
+            trailing: showProfilePicture ? profileButton : nil
+        )
+        .navigationBarBackButtonHidden(true)
+        .background(
             NavigationLink(destination: RegisterView(), isActive: $navigateToRegisterView) {
                 EmptyView()
             }
+        )
+    }
+    private var profileButton: some View {
+        NavigationLink(destination: ProfileView(username: username)) {
+            Image(systemName: "person.circle")
+                .font(.system(size: 28))
         }
     }
-    
     private var backButton: some View {
         Button(action: {
             navigateToRegisterView = true
@@ -207,11 +89,97 @@ struct PlayMenu: View {
         .opacity(showBackButton ? 1.0 : 0.0)
         .disabled(!showBackButton)
     }
-    
-    private var profileButton: some View {
-        NavigationLink(destination: ProfileView(username: username)) {
-            Image(systemName: "person.circle")
-                .font(.system(size: 24))
+}
+struct PlayItemView : View {
+    var data : PlayItem
+    let username: String
+    @Binding var navigateToPlayMenu: Bool
+    var body: some View{
+        HStack(alignment: .top, spacing: 40){
+            Image(self.data.image)
+                .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 175, height: 150)
+            VStack(alignment: .leading, spacing: 10) {
+                Text(self.data.title)
+                    .fontWeight(.bold)
+                Text(self.data.subTitile)
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                HStack(spacing: 50) {
+                    if self.data.title == "Time Limit Easy" {
+                        NavigationLink(destination: generateRandomNoView(shuffle_question_set: 0, username: username)) {
+                            Text("Proceed")
+                                .fontWeight(.bold)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 25)
+                                .background(Color.primary.opacity(0.06))
+                                .clipShape(Capsule())
+                        }
+                    }
+                    if self.data.title == "Time Limit Hard/Heart Attack"{
+                        NavigationLink(destination: generateRandomNoView_time_limit_heart_attack(username: username)) {
+                            Text("Proceed")
+                                .fontWeight(.bold)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 25)
+                                .background(Color.primary.opacity(0.06))
+                                .clipShape(Capsule())
+                        }
+                    }
+                    if self.data.title == "No Time Limit"{
+                        NavigationLink(destination: generateRandomNoView_no_time_limit(shuffle_question_set: 0, username: username)) {
+                            Text("Proceed")
+                                .fontWeight(.bold)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 25)
+                                .background(Color.primary.opacity(0.06))
+                                .clipShape(Capsule())
+                        }
+                    }
+                    if self.data.title == "AR"{
+                        NavigationLink(destination: AR_View()){
+                            Text("Proceed")
+                                .fontWeight(.bold)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 25)
+                                .background(Color.primary.opacity(0.06))
+                                .clipShape(Capsule())
+                        }
+                    }
+                    if self.data.title == "Dictionary"{
+                        NavigationLink(destination: DictionaryView( username: username)){
+                            Text("Proceed")
+                                .fontWeight(.bold)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 25)
+                                .background(Color.primary.opacity(0.06))
+                                .clipShape(Capsule())
+                        }
+                    }
+                    if self.data.title == "Charts"{
+                        NavigationLink(destination: ChartView(username: username)){
+                            Text("Proceed")
+                                .fontWeight(.bold)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 25)
+                                .background(Color.primary.opacity(0.06))
+                                .clipShape(Capsule())
+                        }
+                    }
+                    if self.data.title == "Overview"{
+                        NavigationLink(destination: OverviewView(username: username)){
+                            Text("Proceed")
+                                .fontWeight(.bold)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 25)
+                                .background(Color.primary.opacity(0.06))
+                                .clipShape(Capsule())
+                        }
+                    }
+                }
+            }
+            Spacer(minLength: 0)
         }
     }
 }
@@ -220,3 +188,18 @@ struct PlayMenu_Previews: PreviewProvider {
         WelcomeView()
     }
 }
+struct PlayItem : Identifiable {
+    var id : Int
+    var image : String
+    var title : String
+    var subTitile : String
+}
+var data = [
+    PlayItem(id: 0, image: "time limit easy", title: "Time Limit Easy", subTitile: "Difficulty: 2 star"),
+    PlayItem(id: 1, image: "time limit hard", title: "Time Limit Hard/Heart Attack", subTitile: "Difficulty: 3 star"),
+    PlayItem(id: 2, image: "no time limit", title: "No Time Limit", subTitile: "Difficulty: 1 star"),
+    PlayItem(id: 3, image: "AR", title: "AR", subTitile: "Experience AI generated 3D models and look for defects"),
+    PlayItem(id: 4, image: "Dictionary", title: "Dictionary", subTitile: "Check for previous gameplay records"),
+    PlayItem(id: 5, image: "Charts", title: "Charts", subTitile: "Check for progress along with time"),
+    PlayItem(id: 6, image: "Overview", title: "Overview", subTitile: "Check for gameplay details"),
+]
